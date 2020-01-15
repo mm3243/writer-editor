96d7662d5b0341e15776123112d5d860ff746193
m <for_etc@hotmail.com>
2020-01-15 16:45:25 +0900

    sample作成

---
+++ src/main.js
@@ -0,0 +1,17 @@
+function ShowLength(str, resultid) {
+    // str = str.replace("\n", "");
+    document.getElementById(resultid).innerHTML = str.length + "文字";
+}
+
+var $id = function (id) {
+    return document.getElementById(id);
+};
+
+window.onload = function () {
+    $id("download-link").addEventListener("click", function () {
+        var value = $id("textarea1").value + "\n\n\n\n\n" + $id("textarea2").value;
+        console.log(value);
+        var href = "data:application/octet-stream," + encodeURIComponent(value);
+        this.setAttribute("href", href);
+    }, false);
+}
